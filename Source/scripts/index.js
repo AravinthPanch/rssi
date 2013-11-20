6ab43f06e44e48db023ca22d7049f3840f051c90
Aravinth <aravinth.panchadcharam@yetu.com>
2013-11-20 15:25:16 +0100

    Point Mapping added

--- Source/scripts/index.js
+++ Source/scripts/index.js
@@ -6,5 +6,20 @@ $(function () {
     $("#accordion").accordion({
         active: 0
     });
-
+    setPosition();
 });
+
+
+function setPosition(){
+    $('#ruler').append('<div id="location1" class="pointer"/>')
+    $('#location1').css('left','790px')
+    $('#location1').css('top','0px')
+
+    $('#ruler').append('<div id="location2" class="pointer"/>')
+    $('#location2').css('left','790px')
+    $('#location2').css('top','366px')
+
+    $('#ruler').append('<div id="location3" class="pointer"/>')
+    $('#location3').css('left','0px')
+    $('#location3').css('top','366px')
+}
